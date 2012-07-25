db02f039f48df302ce7b45eb81a629588d5dae05
A.Shaposhnikov <a.shaposhnikov@hh.ru>
2012-07-25 11:01:58 +0400

    fail handling

--- mf.sh
+++ mf.sh
@@ -1,2 +1,7 @@
-mvn-hh clean install -DskipTests -ff -q -T8
-notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/11let/css/hh/blocks/anniversary/logo.png 'Build finished'
+mvn-hh clean install -DskipTests -ff -q -T10
+if [ "$?" = "0" ]; then 
+    notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/11let/css/hh/blocks/anniversary/logo.png 'Build finished'
+else
+    notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/css/ambient/blocks/form/error/triangle.png 'Build faliled miserably'
+    exit 1
+fi
--- mi.sh
+++ mi.sh
@@ -1,2 +1,7 @@
 nice -n 16  mvn-hh clean install org.apache.maven.plugins:maven-eclipse-plugin:2.9:eclipse -ff -T8
-notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/11let/css/hh/blocks/anniversary/logo.png 'Build finished'
+if [ "$?" = "0" ]; then 
+    notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/11let/css/hh/blocks/anniversary/logo.png 'Build finished'
+else
+    notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/css/ambient/blocks/form/error/triangle.png 'Build faliled miserably'
+    exit 1
+fi
