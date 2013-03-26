b48d572989aed8dfd1847bf8e0b106740afe67c8
A.Shaposhnikov <a.shaposhnikov@hh.ru>
2013-03-26 12:00:35 +0400

    m2eclipse integration

--- mi.sh
+++ mi.sh
@@ -1,4 +1,11 @@
-nice -n 16  mvn-hh clean install org.apache.maven.plugins:maven-eclipse-plugin:2.9:eclipse -ff -T8
+grep -l maven.pomderived  .classpath 2>&1 1>/dev/null
+if [ "$?" = "0" ]; then
+    echo "To avoid possible coflict with m2eclipse skipping maven eclipse plugin target. Delete .classpath files manually to do full rebuild"
+    nice -n 16  mvn-hh clean install -ff -T8
+else
+    nice -n 16  mvn-hh clean install org.apache.maven.plugins:maven-eclipse-plugin:2.9:eclipse -ff -T8
+fi
+
 if [ "$?" = "0" ]; then 
     notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/11let/css/hh/blocks/anniversary/logo.png 'Build finished'
 else
