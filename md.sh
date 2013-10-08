455c6bcf024c1cf183c6e09c714e3850c042ba0f
A.Shaposhnikov <a.shaposhnikov@hh.ru>
2013-10-08 09:07:50 +0400

    Fix java options for to build large project

--- md.sh
+++ md.sh
@@ -1,4 +1,4 @@
-nice -n 16  mvn-hh clean install org.apache.maven.plugins:maven-eclipse-plugin:2.9:eclipse -DdownloadSources=true -ff -T8 -U
+MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m" nice -n 16  mvn-hh clean install org.apache.maven.plugins:maven-eclipse-plugin:2.9:eclipse -DdownloadSources=true -ff -T8 -U
 if [ "$?" = "0" ]; then 
     notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/11let/css/hh/blocks/anniversary/logo.png 'Build finished'
 else
