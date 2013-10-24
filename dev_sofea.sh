ebbf91b4da042ca0d952637f1aa9e9e92e8743b7
A.Shaposhnikov <a.shaposhnikov@hh.ru>
2013-10-24 11:00:09 +0400

    dev sofea for quick start

---
+++ dev_sofea.sh
@@ -0,0 +1,18 @@
+cd /home/ashaposhnikov/re/bleeding_edge/sofea-webapp/
+MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=4000,server=y,suspend=n ${MAVEN_OPTS}" nice -n 16 mvn-hh jetty:run &
+sleep 30
+tries=0
+while [ $tries -lt 18 ]; do
+  curl "localhost:8998/status"
+  if [ "${?}" = "0" ]; then
+    break
+  fi
+  sleep 6
+done
+curl "localhost:8998/status"
+if [ "${?}" = "0" ]; then
+    notify-send -i /home/ashaposhnikov/re/bleeding_edge/webapp-static/target/webapp-static/11let/css/hh/blocks/anniversary/logo.png 'Sofea is up' 
+else
+    notify-send 'Sofea failed to run'
+fi
+
